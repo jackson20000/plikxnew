92c9b99fb76ea111db88bf07f31159746be20bc9
Jackson John <jackson20000@gmail.com>
2019-02-08 00:38:32 +0530

    njjbhj

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
+	function __construct() {
+         parent::__construct();
+		 $this->load->helper('url');
+		 $this->load->library('session');
+    }
+
 	public function index()
 	{
 		$this->load->view('index');
@@ -10,6 +16,27 @@ class Home extends CI_Controller {
 	public function signin()
 	{
 		$this->load->view('login_user');
+	}
+	public function join()
+	{
+		$this->load->view('reg');
+	}
+	public function reguser(){
+		$data['fname']=$this->input->post('fname');
+		$data['lname']=$this->input->post('lname');
+		$data['dobd']=$this->input->post('dobd');
+		$data['dobm']=$this->input->post('dobm');
+		$data['doby']=$this->input->post('doby');
+		$data['sex']=$this->input->post('sex');
+		$data['country']=$this->input->post('country');
+		$data['addr1']=$this->input->post('addr1');
+		$data['addr2']=$this->input->post('addr2');
+		$data['city']=$this->input->post('city');
+		$data['state']=$this->input->post('state');
+		$data['zip']=$this->input->post('zip');
+		$data['mob']=$this->input->post('mob');
+		$data['email']=$this->input->post('email');
+		print_r($data);
 	}
 
 }
